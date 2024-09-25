6c10978284bf5b4b8ce2c3ba7cedb54e2ce7b4dd
stafecki <90975231+stafecki@users.noreply.github.com>
2024-09-25 15:42:00 +0200

    Add files via upload

---
+++ project2_dziedziczenie_kompozycja_biblioteka/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2_dziedziczenie_kompozycja_biblioteka
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+
+    public class Author : Person
+    {
+        public List<Book> BookList { get; set; }
+
+        public Author(string firstName, string lastName) : base(firstName, lastName)
+        {
+            BookList = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            BookList.Add(book);
+        }
+
+        public void DisplayBooks()
+        {
+            Console.WriteLine("Lista książek: ");
+            foreach(Book book in BookList)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+    }
+
+    public class Reader : Person
+    {
+        public List<Book> BorrowedBooksList { get; set; }
+        public Reader(string firstName, string lastName) : base(firstName, lastName)
+        {
+            BorrowedBooksList = new List<Book>();
+        }
+        public void BorrowBook(Book book)
+        {
+            BorrowedBooksList.Add(book);
+            Console.WriteLine("Dodano książke o tytule: \"{0}\"", book.Title);
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public int PublicationYear { get; set; }
+        public Book(string title, Author author, int publicationYear)
+        {
+            Title = title;
+            Author = author;
+            PublicationYear = publicationYear;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Author author1 = new Author("Adam", "Mickiewicz");
+            Book book1 = new Book("Pan Tadeusz", author1, 1834);
+            author1.AddBook(book1);
+
+            Reader reader1 = new Reader("Jan", "Kowalski");
+            reader1.BorrowBook(book1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ project2_dziedziczenie_kompozycja_biblioteka/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("project2_dziedziczenie_kompozycja_biblioteka")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("project2_dziedziczenie_kompozycja_biblioteka")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("e0673bfa-1cd6-4347-9384-f1c19eb72a59")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
