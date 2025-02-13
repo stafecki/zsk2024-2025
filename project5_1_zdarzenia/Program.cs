34680db979c82a9f43dcdf2b3195a998e7a5e5ed
Patryk Stafecki <90975231+stafecki@users.noreply.github.com>
2025-02-13 07:55:06 +0100

    Update Program.cs

--- project5_1_zdarzenia/Program.cs
+++ project5_1_zdarzenia/Program.cs
@@ -1,7 +1,7 @@
-﻿using System.Security.Cryptography;
+using System.Security.Cryptography;
 using System.Text;
 
-namespace project5_1_zdarzenia
+namespace _5_1_zdarzenia
 {
     internal class Program
     {
@@ -10,6 +10,14 @@ namespace project5_1_zdarzenia
             Administrator,
             Manager,
             User
+        }
+
+        public enum Permissions
+        {
+            Read,
+            Write,
+            Delete,
+            ManageUsers
         }
 
         public class User
@@ -33,20 +41,20 @@ namespace project5_1_zdarzenia
 
         public class RBAC
         {
-            private readonly Dictionary<Role, List<string>> _rolePermissions;
+            private readonly Dictionary<Role, List<Permissions>> _rolePermissions;
 
 
             public RBAC()
             {
-                _rolePermissions = new Dictionary<Role, List<string>>
+                _rolePermissions = new Dictionary<Role, List<Permissions>>
                 {
-                    { Role.Administrator, new List<string>{ "Read", "Write", "Delete" }},
-                    { Role.Manager, new List<string>{ "Read", "Write" }},
-                    { Role.User, new List<string>{ "Read" } }
+                    { Role.Administrator, new List<Permissions>{ Permissions.Read, Permissions.Write, Permissions.Delete, Permissions.ManageUsers }},
+                    { Role.Manager, new List<Permissions>{ Permissions.Read, Permissions.Write }},
+                    { Role.User, new List<Permissions>{ Permissions.Read } }
                 };
             }
 
-            public bool HasPermission(User user, string permission)
+            public bool HasPermission(User user, Permissions permission)
             {
                 foreach (var role in user.Roles)
                 {
@@ -74,24 +82,23 @@ namespace project5_1_zdarzenia
 
             public static void SavePassword(string username, string password)
             {
-                if (File.ReadLines(_passwordFilePath).Any(line => line.Split(",")[0] == username))
+                if (File.ReadLines(_passwordFilePath).Any(line => line.Split(',')[0] == username))
                 {
                     Console.WriteLine($"Użytkownik {username} już istnieje w systemie");
                     return;
                 }
 
                 string hashedPassword = HashPassword(password);
-
                 File.AppendAllText(_passwordFilePath, $"{username},{hashedPassword}\n");
-                Console.WriteLine($"Użytkownik o nazwie {username} został zapisany");
+                Console.WriteLine($"Użytkownik {username} został zapisany");
             }
 
             public static bool VerifyPassword(string username, string password)
             {
                 string hashedPassword = HashPassword(password);
-                foreach(var line in File.ReadLines(_passwordFilePath))
+                foreach (var line in File.ReadLines(_passwordFilePath))
                 {
-                    var parts = line.Split(",");
+                    var parts = line.Split(',');
                     if (parts[0] == username && parts[1] == hashedPassword)
                     {
                         PasswordVerified?.Invoke(username, true);
@@ -112,19 +119,96 @@ namespace project5_1_zdarzenia
             }
         }
 
+        public delegate void FileOperetions(string filePath);
+
+        public class FileManager()
+        {
+            //dokończyć
+        }
+
         static void Main(string[] args)
         {
-            PasswordManager.PasswordVerified += (username, success) => Console.WriteLine($"Logowanie użytkownika o nazwie {username} : {(success ? "udane" : "nieudane")}");
+            //PasswordManager.PasswordVerified += (username, success) => Console.WriteLine($"Logowanie użytkownika {username} : {(success ? "udane" : "nieudane")}");
 
-            PasswordManager.SavePassword("Admin", "adminPassword");
+            PasswordManager.SavePassword("AdminUser", "adminPassword");
+            PasswordManager.SavePassword("ManagerUser", "managerPassword");
+            PasswordManager.SavePassword("NormalUser", "userPassword");
+            PasswordManager.SavePassword("xyz", "userPassword");
 
-            Console.Write("\nWprowadź nazwę użytkownika: ");
+            bool exitProgram = false;
+
+            while(!exitProgram)
+            {
+                Console.Clear();
+                Console.WriteLine("=== SYSTEM LOGOWANIA ===");
+                Console.Write("Wprowadź nazwę użytkownika: ");
+                string username = Console.ReadLine();
+                Console.Write("Wprowadź hasło użytkownika: ");
+                string password = Console.ReadLine();
+
+                if(!PasswordManager.VerifyPassword(username, password))
+                {
+                    Console.WriteLine("Niepoprawna nazwa użytkownika lub hasło");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                var user = new User(username);
+
+                if (username == "AdminUser") user.AddRole(Role.Administrator);
+                else if (username == "ManagerUser") user.AddRole(Role.Manager);
+                else if (username == "NormalUser") user.AddRole(Role.User);
+
+                var rbacSystem = new RBAC();
+
+                string filePath = "testFile.txt";
+
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Zalogowano jako: {username}");
+                    Console.WriteLine($"\nWybierz opcję: ");
+                    Console.WriteLine("1. Odczytaj plik");
+                    if (rbacSystem.HasPermission(user, Permissions.Write))
+                    {
+                        Console.WriteLine("2. Zapisz do pliku");
+                    }
+                    if (rbacSystem.HasPermission(user,Permissions.Delete))
+                    {
+                        Console.WriteLine("3. Modyfikuj plik");
+                    }
+                    if (rbacSystem.HasPermission(user, Permissions.ManageUsers))
+                    {
+                        Console.WriteLine("4. Dodaj nowego użytkownika");
+                    }
+                    Console.WriteLine("5. Wyloguj się");
+                    Console.WriteLine("6. Wyjdź z porgramu");
+                    Console.ReadKey();
+
+                    int choice;
+                    if(!int.TryParse(Console.ReadLine(), out choice))
+                    {
+                        Console.WriteLine("Nieprawny wybór spróbuj ponownie");
+                        continue;
+                    }
+
+                    switch (choice)
+                    {
+                        //case 1: dokonczyc
+                    }
+
+                    Console.ReadKey();
+                }
+            }
+
+           /* Console.Write("\nWprowadź nazwę użytkownika: ");
             string username = Console.ReadLine();
 
-            Console.WriteLine("Wprowadź hasło: ");
+            Console.Write("Wprowadź hasło: ");
             string password = Console.ReadLine();
+            Console.WriteLine();
 
-            if(!PasswordManager.VerifyPassword(username, password))
+            if (!PasswordManager.VerifyPassword(username, password))
             {
                 Console.WriteLine("Niepoprawna nazwa użytkownika lub hasło");
                 return;
@@ -132,15 +216,18 @@ namespace project5_1_zdarzenia
 
             var user = new User(username);
 
-            if(username == "AdminUser")
+            if (username == "AdminUser")
             {
                 user.AddRole(Role.Administrator);
             }
 
-            var rbacSstem = new RBAC();
+            var rbacSystem = new RBAC();
 
             Console.WriteLine("\nSprawdzenie dostępu do różnych zasobów:");
-            Console.WriteLine("Read: " + rbacSstem.HasPermission(user, "Read"));
+
+            Console.WriteLine("Read: " + rbacSystem.HasPermission(user, "Read"));
+            Console.WriteLine("Write: " + rbacSystem.HasPermission(user, "Write"));
+            Console.WriteLine("Delete: " + rbacSystem.HasPermission(user, "Delete"));*/
         }
     }
 }
